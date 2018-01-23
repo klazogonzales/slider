05376c2c824b723f387afbb048eeae28a776fbc9
Kimberlin Lazo Gonzales <klazogonzales@wlapcs.org>
2018-01-23 19:05:58 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -1,12 +1,28 @@
+var redSlider;
+var greenSlider;
+var blueSlider;
+
 function setup(){
     createCanvas(600,400);
     background(0);
+    
+    //createSlider(min,max,default);
+    redSlider = createSlider(0,255,100);
+    //.position(x,y)
+    redSlider.position(20,20);
+    
+    greenSlider = createSlider(0,255,100);
+    greenSlider.position(20,50);
+    
+    blueSlider = createSlider(0,255,100);
+    blueSlider.position(20,80);
 }
 
 
-
-
-
 function draw(){
+    redValue = redSlider.value();
+    greenValue = greenSlider.value();
+    blueValue = blueSlider.value();
     
+    background(redValue, greenValue, blueValue);
 }
